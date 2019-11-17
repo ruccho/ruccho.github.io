50b54e5c65814667d7a13dd25d92fd9cdc6e264b
ruccho <noboroo2000@yahoo.co.jp>
2019-11-17 17:58:11 +0900

    add chikaro pages

---
+++ chikaro/js/common.js
@@ -0,0 +1,62 @@
+$(function() {
+    $('.menu .menu-trigger').on('click', function() {
+        $(this).toggleClass('active');
+        $('.menu-list').toggleClass('active');
+        $('.menu').toggleClass('active');
+        return false;
+    });
+    var sections = [
+        "main",
+        "story",
+        "chara",
+        "video",
+        "dl"
+    ]
+
+    $('.menu .menu-item-main').on('click', function() {
+        activateSection('main');
+        return false;
+    });
+
+    $('.menu .menu-item-story').on('click', function() {
+        activateSection('story');
+        return false;
+    });
+
+    $('.menu .menu-item-chara').on('click', function() {
+        activateSection('chara');
+        return false;
+    });
+    $('.menu .menu-item-video').on('click', function() {
+        activateSection('video');
+        return false;
+    });
+    $('.menu .menu-item-dl').on('click', function() {
+        activateSection('dl');
+        return false;
+    });
+
+    function activateSection(section) {
+
+        $('.menu .menu-trigger').toggleClass('active');
+        $('.menu-list').toggleClass('active');
+        $('.menu').toggleClass('active');
+
+        for (var i = 0; i < sections.length; i++) {
+            if (sections[i] == section) {
+                $('.' + sections[i]).addClass('active');
+                $('.' + sections[i]).removeClass('inactive');
+                $('.menu-item-' + sections[i]).addClass('menu-item-selected');
+            } else {
+                $('.' + sections[i]).removeClass('active');
+                $('.' + sections[i]).addClass('inactive');
+                $('.menu-item-' + sections[i]).removeClass('menu-item-selected');
+            }
+        }
+        if (section == 'main') {
+            $('.menu-item-main').addClass('menu-item-selected');
+        } else {
+            $('.menu-item-main').removeClass('menu-item-selected');
+        }
+    }
+});
